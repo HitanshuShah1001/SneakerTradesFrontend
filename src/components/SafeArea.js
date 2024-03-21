c901724e835617f7cd584f4e8fde08e9dbd4b5cf
Hitanshu Shah <hitanshushah5@gmail.com>
2024-03-21 23:49:04 +1000

    Loading indicator added

--- src/components/SafeArea.js
+++ src/components/SafeArea.js
@@ -1,9 +1,48 @@
-import {SafeAreaView} from 'react-native';
+import React from 'react';
+import {ActivityIndicator, SafeAreaView, View, StyleSheet} from 'react-native';
 import {Header} from './Header';
+import {useContext} from 'react';
+import {Context} from '../navigation/BottomTab';
 
-export const SafeArea = ({children, go_back = false, text}) => (
-  <SafeAreaView style={{flex: 1}}>
-    <Header go_back={go_back} text={text} />
-    {children}
-  </SafeAreaView>
-);
+const LoadingIndicator = () => {
+  return (
+    <View style={[styles.loadingContainer, StyleSheet.absoluteFillObject]}>
+      <ActivityIndicator size={'large'} />
+    </View>
+  );
+};
+
+export const SafeArea = ({children, go_back = false, text}) => {
+  const {loading} = useContext(Context);
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <Header go_back={go_back} text={text} />
+      {loading && <LoadingIndicator />}
+      <View
+        style={[
+          styles.content,
+          loading && styles.blur,
+          loading && styles.noInteraction,
+        ]}>
+        {children}
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    zIndex: 2,
+  },
+  content: {
+    flex: 1,
+    padding: 16,
+  },
+  noInteraction: {
+    pointerEvents: 'none',
+  },
+});
