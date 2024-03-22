1ca6b75c79e3b47326678f481ee20c38bee2e0b1
Hitanshu Shah <hitanshushah5@gmail.com>
2024-03-22 22:10:30 +1000

    Requests added

--- src/screens/requests/index.js
+++ src/screens/requests/index.js
@@ -1,20 +1,21 @@
-import React, {useEffect, useState} from 'react';
-import {dummySneakerData} from '../../dummydata/Sneakers';
-import {Scroller} from '../../components/Scroller';
+import React, {useContext, useEffect, useState} from 'react';
 import Sneakercard from '../../components/Sneakercard';
 import {useNavigation} from '@react-navigation/native';
-import {SNEAKER_DETAIL} from '../../constants/Screen';
 import {apiService} from '../../services/apiService';
 import {RetrieveTokenFromLocalStorage} from '../../utils/GetDeleteStoreTokenInLocalStorage';
 import {FlatList} from 'react-native';
 import {SafeArea} from '../../components/SafeArea';
+import {Context} from '../../navigation/BottomTab';
+
 export const Requests = () => {
   const navigation = useNavigation();
   const [sneakers, setSneakers] = useState([]);
   const [page, setPage] = useState(1);
   const [limit, setLimit] = useState(10);
+  const {loading, setLoading} = useContext(Context);
 
   const getSneakersForRental = async () => {
+    setLoading(true);
     let token = await RetrieveTokenFromLocalStorage();
     const response = await apiService.get(
       `sneakerrequests/requests?page=${page}&limit=${limit}`,
@@ -22,13 +23,13 @@ export const Requests = () => {
         Authorization: `Bearer ${token}`,
       },
     );
-
+    setLoading(false);
     setSneakers(response.data);
   };
 
   useEffect(() => {
     getSneakersForRental();
-  }, []);
+  }, [page]);
 
   return (
     <SafeArea>
@@ -48,6 +49,12 @@ export const Requests = () => {
               }
             />
           );
+        }}
+        onEndReachedThreshold={0.01}
+        onEndReached={() => {
+          if (!loading) {
+            setPage(prevPage => prevPage + 1); // Increment page only if not already loading
+          }
         }}
       />
     </SafeArea>
