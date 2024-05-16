87d801b0698b4349cade306ad22af4e2806d938c
Hitanshu Shah <hitanshushah5@gmail.com>
2024-05-16 12:07:10 +0530

    Api param changed to heroku

--- src/constants/Apicall.js
+++ src/constants/Apicall.js
@@ -4,7 +4,6 @@ export const UPDATE_PROFILE_CALL = 'user/update';
 export const RESET_PASSWORD_CALL = 'user/resetpassword';
 export const UPLOAD_CALL = 'sneaker/upload';
 export const UPLOAD_REQUEST_CALL = 'sneakerrequests/createrequest';
-export const GET_SNEAKER_REQUESTS = `sneakerrequests/requests`;
 export const GET_SNEAKERS_OWNED = `sneaker/getsneakersowned`;
 export const CHECK_IF_USERNAME_EMAIL_PHONE_EXISTS = `user/usernameemailphoneexists`;
 export const SEND_OTP_EMAIL = `user/sendemail`;
